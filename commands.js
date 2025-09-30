9a04e9840ccea5f3b3bea3fbf0cfacd6d471c332
Torri-FVD <torris@fvdublin.org>
2025-09-30 11:17:08 -0400

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,6 @@
+function openFormInBrowser(event) {
+    var formUrl = "https://forms.office.com/pages/responsepage.aspx?id=hw-wQpoTYEuwt37ahz0ZMC759A5Kw6dEjMZJErw85gdUQlFJWllTM1ZEWlRWOVRRQkFGQ0tNMTU1Ri4u";
+    window.open(formUrl, "_blank");
+    event.completed();
+}
+
